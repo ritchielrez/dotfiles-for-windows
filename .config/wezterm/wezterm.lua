2fe6a4e88183121dbbfee3508ec590ee7389f428
RitchielRez <ritchielrez@gmail.com>
2022-01-22 15:29:46 +0600

    Changed my wezterm font to JetBrainsMono

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ return {
     default_prog = {"C:\\Program Files\\PowerShell\\7\\pwsh.exe"},
     color_scheme_dirs = {"C:\\Program Files\\WezTerm\\colorschemes"},
     color_scheme = "Gruvbox Dark",
-    font = wezterm.font("CaskaydiaCove NF"),
+    font = wezterm.font("JetBrainsMono NF"),
     font_size = 12.5,
     window_background_opacity = 0.9,
     enable_tab_bar = false,
