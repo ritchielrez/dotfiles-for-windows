1458ad418c8c51c96d6a2e2824ba22b56c30b831
RitchielRez <ritchielrez@gmail.com>
2022-01-24 17:55:23 +0600

    I wanna pick what I want to launch, wsl or powershell

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,11 @@
 local wezterm = require 'wezterm';
 return {
-    default_prog = {"C:\\Program Files\\PowerShell\\7\\pwsh.exe"},
-    color_scheme_dirs = {"C:\\Program Files\\WezTerm\\colorschemes"},
+--    default_prog = {"C:\\Program Files\\PowerShell\\7\\pwsh.exe"},
     color_scheme = "Gruvbox Dark",
     font = wezterm.font("JetBrainsMono NF"),
     font_size = 12.5,
     window_background_opacity = 0.9,
     enable_tab_bar = false,
+    
 }
 
