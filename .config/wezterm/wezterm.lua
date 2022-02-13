8e198a938c7a734e7ec67f75cbbeff7a7133d2d1
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2022-02-13 13:39:29 +0600

    Wezterm config update with new colorscheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,18 @@
-local wezterm = require 'wezterm';
-return {
---    default_prog = {"C:\\Program Files\\PowerShell\\7\\pwsh.exe"},
-    color_scheme = "Gruvbox Dark",
-    font = wezterm.font("JetBrainsMono NF"),
-    font_size = 12.15,
-    window_background_opacity = 0.95,
-    enable_tab_bar = false,
-    
-}
+local wezterm = require("wezterm")
 
+-- local scheme = wezterm.get_builtin_color_schemes()["Gruvbox Dark"]
+-- scheme.background = "#282828"
+-- scheme.foreground = "#ebdbb2"
+
+return {
+	--    default_prog = {"C:\\Program Files\\PowerShell\\7\\pwsh.exe"},
+	-- color_schemes = {
+	-- 	-- Override the builtin Gruvbox Light scheme with our modification.
+	-- 	["Gruvbox Dark"] = scheme,
+	-- },
+	color_scheme = "Gruvbox",
+	font = wezterm.font("CaskaydiaCove NF"),
+	font_size = 12.15,
+	window_background_opacity = 0.96,
+	enable_tab_bar = false,
+}
