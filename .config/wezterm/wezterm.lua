3b34920f96c1152a4434a9202fa1e4143a531b92
RitchielRez <ritchielrez@gmail.com>
2022-04-28 21:18:51 +0600

    fix(wezterm): removed extra comments

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,7 @@
 local wezterm = require("wezterm")
 
 return {
-	--    default_prog = {"C:\\Program Files\\PowerShell\\7\\pwsh.exe"},
-	-- color_schemes = {
-	-- 	-- Override the builtin Gruvbox Light scheme with our modification.
-	-- 	["Gruvbox Dark"] = scheme,
-	-- },
+	-- default_prog = {"C:\\Program Files\\PowerShell\\7\\pwsh.exe"},
 
 	skip_close_confirmation_for_processes_named = {
 		"bash",
@@ -18,10 +14,12 @@ return {
 		"pwsh.exe",
 		"powershell.exe",
 		"nvim.exe",
+		"vim.exe",
+		"bash.exe",
 	},
 	font = wezterm.font("CaskaydiaCove NF"),
-	font_size = 12.15,
-	-- window_background_opacity = 0.96,
+	font_size = 13,
+	--window_background_opacity = 0.96,
 	color_scheme = "Catppuccin",
 	enable_tab_bar = false,
 }
