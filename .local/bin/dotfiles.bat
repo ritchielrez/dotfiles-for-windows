1166bdc75c08637907ae94b4c3fbc25d5fa74817
RitchielRez <52417892+ritchielrez@users.noreply.github.com>
2023-08-13 16:08:36 +0600

    fix(dotfiles): a better way to use yadm in cmd

--- .local/bin/dotfiles.bat
+++ .local/bin/dotfiles.bat
@@ -1,2 +1,11 @@
 @echo off
-bash yadm %*
+REM Tell git where is our yadm dotfiles repo is stored
+set "GIT_DIR=%USERPROFILE%\.local\share\yadm\repo.git"
+
+REM Except any number arguments passed to this batch file,
+REM then pass it to git
+git %*
+
+REM Now remove the previously created GIT_DIR, as hard coding
+REM the GIT_DIR path does not allow us to run git commands for other git repos
+set "GIT_DIR="
